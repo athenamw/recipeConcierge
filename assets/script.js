97c78bb99340c9c278c5c7a02dc803e591923e24
Athena <wallisathena@gmail.com>
2023-06-08 16:58:49 -0700

    pull random recipe

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-const randomRecipe = "www.themealdb.com/api/json/v1/1/random.php";
-var search = document.getElementById("#searchBtn");
+const randomRecipeApi = "https://www.themealdb.com/api/json/v1/1/random.php";
+const search = document.getElementById("#searchBtn");
+const daily = document.getElementById("daily");
+const randomDaily = document.getElementById("randomRecipe");
 
-search.addEventListener("click", function () {});
+daily.addEventListener("click", () => {
+  fetch(randomRecipeApi)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res.meals[0]);
+    });
+});
