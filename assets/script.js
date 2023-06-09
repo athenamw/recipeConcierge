fde44f012cb615a5a55f8e9850bba2b7a2abab88
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-06-08 21:14:11 -0700

    added the search function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 const randomRecipeApi = "https://www.themealdb.com/api/json/v1/1/random.php";
-const search = document.getElementById("#searchBtn");
+const search = document.getElementById("searchBtn");
 const daily = document.getElementById("daily");
 const randomDaily = document.getElementById("randomRecipe");
+var input = document.getElementById("searchText").value;
 
 daily.addEventListener("click", function () {
   fetch(randomRecipeApi).then(function (response) {
@@ -13,4 +14,36 @@ daily.addEventListener("click", function () {
   });
 });
 
-const generateMeal = function (meal) {};
+const generateMeal = function (input) {};
+
+var getSearchresults = function() {
+  //will grab the input from the user for the search
+  var input = document.getElementById("searchText").value;
+
+  //localStorage.setItem("searchText",input);
+
+  var mainSearchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + input;
+  //calls the api
+  fetch(mainSearchApi)
+  .then(function(response) {
+    if (response.ok){
+      response.json().then(function(data) {
+        console.log(data);
+
+      });
+      //will run if it cant call the api
+    } else {
+      console.log("error");
+    }
+  })
+  //will run if it cant connect to the server
+  .catch(function(error) {
+    console.log("error");
+  });
+  //console logs th user input
+  console.log(input);
+};
+
+search.addEventListener("click", function (){
+getSearchresults();
+});
