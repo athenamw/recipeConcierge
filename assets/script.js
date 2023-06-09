3c7fbe92e8215615954d9f423c172cab1b543be7
athenamw <wallisathena@gmail.com>
2023-06-09 11:54:42 -0700

    created ingredient list for each random recipe

--- assets/script.js
+++ assets/script.js
@@ -3,31 +3,39 @@ const search = document.getElementById("#searchBtn");
 const dailyBtn = document.getElementById("daily");
 const randomDaily = document.getElementById("randomRecipe");
 var recipeName = document.getElementById("recipeName");
+var img = document.getElementById("image");
+var ingredients = document.getElementById("ingredients");
 
 function getRandomRecipe() {
+  ingredients.innerHTML = "Ingredients";
   fetch(randomRecipeApi).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data.meals);
         var recipe = data.meals[0];
-        var recipeName = document.getElementById("recipeName");
+        console.log(recipe);
         recipeName.textContent = recipe.strMeal;
-        var img = document.getElementById("image");
         img.src = recipe.strMealThumb;
-        var ingredients = document.getElementById("ingredients");
-        // ingredients.textContent =
-        //   recipe.strMeasure1.trim() + " " + recipe.strIngredient1.trim();
         var ingredientList = recipe.strMeasure;
         var measure = recipe.strIngredient;
-        console.log(ingredientList);
         console.log(measure);
         for (let i = 1; i <= 20; i++) {
-          ingredients.textContent = measure + " " + ingredientList;
+          var ingredientKey = "strIngredient" + i;
+          var ingredientValue = recipe[ingredientKey];
+          console.log(ingredientValue);
+          var measureKey = "strMeasure" + i;
+          var measureValue = recipe[measureKey];
+          console.log(measureValue);
+
+          if (ingredientValue && measureValue) {
+            ingredients.innerHTML += `<p>${measureValue} of ${ingredientValue}</p>`;
+          }
         }
       });
     }
   });
 }
+
 window.addEventListener("load", getRandomRecipe);
 dailyBtn.addEventListener("click", getRandomRecipe);
 
