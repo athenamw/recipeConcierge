4123bd98a628f6f4c0aa8d16b338102e6416828e
athenamw <wallisathena@gmail.com>
2023-06-09 10:48:05 -0700

    trying to get loop to work to list ingredients

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const randomRecipeApi = "https://www.themealdb.com/api/json/v1/1/random.php";
 const search = document.getElementById("#searchBtn");
 const dailyBtn = document.getElementById("daily");
 const randomDaily = document.getElementById("randomRecipe");
+var recipeName = document.getElementById("recipeName");
 
 function getRandomRecipe() {
   fetch(randomRecipeApi).then(function (response) {
@@ -9,9 +10,20 @@ function getRandomRecipe() {
       response.json().then(function (data) {
         console.log(data.meals);
         var recipe = data.meals[0];
-        var img = document.createElement("img");
+        var recipeName = document.getElementById("recipeName");
+        recipeName.textContent = recipe.strMeal;
+        var img = document.getElementById("image");
         img.src = recipe.strMealThumb;
-        document.body.appendChild(img);
+        var ingredients = document.getElementById("ingredients");
+        // ingredients.textContent =
+        //   recipe.strMeasure1.trim() + " " + recipe.strIngredient1.trim();
+        var ingredientList = recipe.strMeasure;
+        var measure = recipe.strIngredient;
+        console.log(ingredientList);
+        console.log(measure);
+        for (let i = 1; i <= 20; i++) {
+          ingredients.textContent = measure + " " + ingredientList;
+        }
       });
     }
   });
