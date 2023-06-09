b4824b19b95d0c65dc16c99a8444a8cae5407371
athenamw <wallisathena@gmail.com>
2023-06-08 20:51:58 -0700

    created daily function

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,16 @@ const search = document.getElementById("#searchBtn");
 const daily = document.getElementById("daily");
 const randomDaily = document.getElementById("randomRecipe");
 
+function daily() {
+  fetch(randomRecipeApi).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data.meals);
+      });
+    }
+  });
+}
+
 daily.addEventListener("click", function () {
   fetch(randomRecipeApi).then(function (response) {
     if (response.ok) {
