b92665c3a65e1aec1a2731641e0c307eea89932e
athenamw <wallisathena@gmail.com>
2023-06-09 09:08:26 -0700

    Started to make recipe card format

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,22 @@
 const randomRecipeApi = "https://www.themealdb.com/api/json/v1/1/random.php";
 const search = document.getElementById("#searchBtn");
-const daily = document.getElementById("daily");
+const dailyBtn = document.getElementById("daily");
 const randomDaily = document.getElementById("randomRecipe");
 
-function daily() {
+function getRandomRecipe() {
   fetch(randomRecipeApi).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data.meals);
+        var recipe = data.meals[0];
+        var img = document.createElement("img");
+        img.src = recipe.strMealThumb;
+        document.body.appendChild(img);
       });
     }
   });
 }
-
-daily.addEventListener("click", function () {
-  fetch(randomRecipeApi).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data.meals);
-      });
-    }
-  });
-});
+window.addEventListener("load", getRandomRecipe);
+dailyBtn.addEventListener("click", getRandomRecipe);
 
 const generateMeal = function (meal) {};
